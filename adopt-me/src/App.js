06307a2e8afc100ccb9f380c9664457d322b5eee
Alonso Araya Calvo <alonso@elumen.info>
2021-02-01 19:20:59 -0600

    Pure React Finished

---
+++ adopt-me/src/App.js
@@ -0,0 +1,32 @@
+// Your code is going to go here
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatie;',
+    }),
+    React.createElement(Pet, {
+      name: 'Doink',
+      animal: 'Cat',
+      breed: 'Mixed',
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
