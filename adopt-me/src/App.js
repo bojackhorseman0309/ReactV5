7f33d2069b8f4827d7fe45e468e7f441c690fd59
Alonso Araya Calvo <alonso@elumen.info>
2021-02-08 20:24:15 -0600

    Hooks Module Finished

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,32 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatie;",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed",
-  //   }),
-  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
-      <Pet name="Doink" animal="Cat" breed="Mixed"></Pet>
+      <SearchParams></SearchParams>
     </div>
   );
 };
---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown></AnimalDropdown>
+        <BreedDropdown></BreedDropdown>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
