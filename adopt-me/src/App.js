0c748e6e75df548393d76a3b1a73da9a7377db9f
Alonso Araya Calvo <alonso@elumen.info>
2021-02-09 10:46:22 -0600

    Async Routing module finished

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import { Link, Router } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams></SearchParams>
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/"></SearchParams>
+          <Details path="/details/:id"></Details>
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
